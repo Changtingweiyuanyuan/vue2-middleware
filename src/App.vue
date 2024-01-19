c323a97f674f8baf480eee3df078d754ecefefde
chelsea.chang <yuan097@kimo.com>
2024-01-19 17:35:25 +0800

    [style] 調整App.vue nav及footer樣式

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app">
-    <router-link class="router-link" to="/">會員資訊</router-link>｜
-    <router-link class="router-link" to="/books">所有書籍資訊</router-link>｜
-    <router-link class="router-link" to="/rentedBooks"
-      >已租借書籍資訊</router-link
-    >
-    <router-view />
+  <div id="app" class="d-flex flex-column">
+    <div class="nav h5">
+      <div class="nav__logo px-4 py-1">
+        <img src="@/assets/logo.png" />
+      </div>
+      <router-link class="router-link px-4 py-2" to="/">會員資訊</router-link>
+      <router-link class="router-link px-4 py-2" to="/books"
+        >所有書籍資訊</router-link
+      >
+      <router-link class="router-link px-4 py-2" to="/rentedBooks"
+        >已租借書籍資訊</router-link
+      >
+    </div>
+    <div class="main flex-grow-1 container my-2">
+      <router-view />
+    </div>
+    <div class="footer mx-4 my-2 h6">
+      <marquee>文字</marquee>
+    </div>
   </div>
 </template>
 
@@ -18,12 +30,36 @@ export default {
 
 <style lang="scss" scoped>
 @import "vue2-components.styles/global.import.scss";
+
+$background: #eef7f2;
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: $gray-900;
+  height: 100vh;
+  background: $background;
+
+  .nav {
+    border-bottom: 1px solid $gray-400;
+    & > * {
+      border-right: 1px solid $gray-400;
+    }
+
+    &__logo img {
+      width: 30px;
+      height: 30px;
+    }
+  }
+
+  .footer {
+    color: $gray-600;
+  }
+
+  a {
+    color: $gray-900;
+    text-decoration: none;
+  }
 }
 </style>
